41d590f12dc0a3692f8fbae6971efaf1d56b0bfd
Dima Zhiltsov <zhiltsovdima@gmail.com>
2022-10-28 18:50:09 +0300

    Initial Commit

---
+++ Secret GardenUITests/Secret_GardenUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Secret_GardenUITestsLaunchTests.swift
+//  Secret GardenUITests
+//
+//  Created by Dima Zhiltsov on 28.10.2022.
+//
+
+import XCTest
+
+final class Secret_GardenUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
