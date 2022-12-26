d67bdf346f32ca22e598ee208bd6db8beeb1cb42
Dima Zhiltsov <zhiltsovdima@gmail.com>
2022-12-26 18:23:06 +0300

    New Features model for the plant

--- Secret Garden/Model/Plant.swift
+++ Secret Garden/Model/Plant.swift
@@ -11,17 +11,12 @@ struct Plant: Codable {
     var name: String
     var imageData: PlantImageData
     
-    var features: PlantFeatures?
+    var features: Features?
     
     init(name: String, image: PlantImageData) {
         self.name = name
         self.imageData = image
     }
-}
-
-struct PlantFeatures: Codable {
-    var latinName: String
-    var values: [String]
 }
 
 struct PlantImageData: Codable {
@@ -33,5 +28,67 @@ struct PlantImageData: Codable {
     
     init(_ image: UIImage) {
         self.imageData = image.jpegCompress(.low)
+    }
+}
+
+struct Features {
+    let latinName: String
+    let origin: String
+    let idealLight: String
+    let watering: String
+    let insects: [String]
+    
+    let tempMin: Int
+    let tempMax: Int
+    
+    var temperature: String {
+        "From \(tempMin) to \(tempMax)"
+    }
+}
+
+extension Features: Codable {
+    
+    enum CodingKeys: String, CodingKey {
+        case latinName = "latin"
+        case origin
+        case idealLight = "ideallight"
+        case watering
+        case insects
+        
+        case tempMin = "tempmin"
+        case tempMax = "tempmax"
+    }
+    
+    enum TempCodingKeys: String, CodingKey {
+        case celsius
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.latinName = try container.decode(String.self, forKey: .latinName)
+        self.origin = try container.decode(String.self, forKey: .origin)
+        self.idealLight = try container.decode(String.self, forKey: .idealLight)
+        self.watering = try container.decode(String.self, forKey: .watering)
+        self.insects = try container.decode([String].self, forKey: .insects)
+        
+        //Nested Containers
+        let tempContainer = try container.nestedContainer(keyedBy: TempCodingKeys.self, forKey: .tempMin)
+        let tempMaxContainer = try container.nestedContainer(keyedBy: TempCodingKeys.self, forKey: .tempMax)
+        self.tempMin = try tempContainer.decode(Int.self, forKey: .celsius)
+        self.tempMax = try tempMaxContainer.decode(Int.self, forKey: .celsius)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(latinName, forKey: .latinName)
+        try container.encode(origin, forKey: .origin)
+        try container.encode(idealLight, forKey: .idealLight)
+        try container.encode(watering, forKey: .watering)
+        try container.encode(insects, forKey: .insects)
+        
+        var tempMinContainer = container.nestedContainer(keyedBy: TempCodingKeys.self, forKey: .tempMin)
+        var tempMaxContainer = container.nestedContainer(keyedBy: TempCodingKeys.self, forKey: .tempMax)
+        try tempMinContainer.encode(tempMin, forKey: .celsius)
+        try tempMaxContainer.encode(tempMax, forKey: .celsius)
     }
 }
