c8eea097b94aab7bb02cb1ad3f8bba43f83ad9f5
Dima Zhiltsov <zhiltsovdima@gmail.com>
2023-01-09 23:53:02 +0300

    Adding new functions in the Assembly and the Coordinator

--- Secret Garden/Application/Assembly.swift
+++ Secret Garden/Application/Assembly.swift
@@ -9,21 +9,40 @@ import UIKit
 
 protocol AssemblyProtocol {
     func createHome() -> UIViewController
-    func createPlants() -> UIViewController
+    func createPlants(output: PlantsOutput) -> UIViewController
     func createShop() -> UIViewController
+    
+    func createAddNewPlant() -> UIViewController
+    func createDetailPlant(_ plant: Plant) -> UIViewController
+
 }
 
 class Assembly: AssemblyProtocol {
+    
+    var garden = Garden()
     
     func createHome() -> UIViewController {
         return HomeViewController()
     }
     
-    func createPlants() -> UIViewController {
-        return PlantsViewController()
+    func createPlants(output: PlantsOutput) -> UIViewController {
+        let viewModel = PlantsViewModel(output: output, garden: garden)
+        let view = PlantsViewController1(viewModel: viewModel)
+        return view
     }
     
     func createShop() -> UIViewController {
         return ShopViewController()
+    }
+    
+    func createAddNewPlant() -> UIViewController {
+        let addNewPlantView = AddPlantController()
+        return addNewPlantView
+    }
+    
+    func createDetailPlant(_ plant: Plant) -> UIViewController {
+        let detailPlantView = DetailPlantController()
+        detailPlantView.setPlant(plant)
+        return detailPlantView
     }
 }
--- Secret Garden/Application/Coordinator.swift
+++ Secret Garden/Application/Coordinator.swift
@@ -16,6 +16,8 @@ class Coordinator: CoordinatorProtocol {
     private let assembly: AssemblyProtocol
     
     private var tabViewController: TabBarController!
+    private var plantsNavigationController: NavBarController!
+    private var shopNavigationController: NavBarController!
     
     init(_ assembly: AssemblyProtocol) {
         self.assembly = assembly
@@ -23,13 +25,16 @@ class Coordinator: CoordinatorProtocol {
     
     func start(window: UIWindow) {
         let homeView = assembly.createHome()
-        let plantsView = assembly.createPlants()
+        let plantsView = assembly.createPlants(output: self)
         let shopView = assembly.createShop()
+        
+        plantsNavigationController = NavBarController(rootViewController: plantsView)
+        shopNavigationController = NavBarController(rootViewController: shopView)
         
         let controllers = [
             homeView,
-            NavBarController(rootViewController: plantsView),
-            NavBarController(rootViewController: shopView)
+            plantsNavigationController!,
+            shopNavigationController!
         ]
         
         tabViewController = TabBarController(controllers)
@@ -37,4 +42,20 @@ class Coordinator: CoordinatorProtocol {
         window.rootViewController = tabViewController
         window.makeKeyAndVisible()
     }
+}
+
+// MARK: - PlantOutput
+
+extension Coordinator: PlantsOutput {
+    
+    func showAddNewPlant() {
+        let addNewPlantView = assembly.createAddNewPlant()
+        plantsNavigationController.pushViewController(addNewPlantView, animated: true)
+    }
+    
+    func showPlantDetail(_ plant: Plant) {
+        let detailPlantView = assembly.createDetailPlant(plant)
+        plantsNavigationController.pushViewController(detailPlantView, animated: true)
+    }
+    
 }
