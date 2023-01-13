ee999e3c6218bb8bcdbeb4cd0d9a110e5ec40bb2
Dima Zhiltsov <zhiltsovdima@gmail.com>
2023-01-13 18:49:45 +0300

    DetailPlantCoordinator implementation

---
+++ Secret Garden/Modules/Garden/DetailPlant/DetailPlantCoordinator.swift
@@ -0,0 +1,34 @@
+//
+//  DetailPlantCoordinator.swift
+//  Secret Garden
+//
+//  Created by Dima Zhiltsov on 13.01.2023.
+//
+
+import UIKit.UINavigationController
+
+final class DetailPlantCoordinator: Coordinator {
+    
+    private(set) var childCoordinators: [Coordinator] = []
+    
+    private let navigationController: UINavigationController
+    private let garden: Garden
+    private let index: Int
+        
+    init(_ navigationController: UINavigationController, _ garden: Garden, index: Int) {
+        self.navigationController = navigationController
+        self.garden = garden
+        self.index = index
+    }
+    
+    func start() {
+        let viewModel = DetailPlantViewModel(output: self, garden, index)
+        let detailView = DetailPlantController(viewModel: viewModel)
+        navigationController.pushViewController(detailView, animated: true)
+    }
+}
+
+extension DetailPlantCoordinator: DetailPlantOutput {
+    
+
+}
