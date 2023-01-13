301161517c32ec56d07a9c88f31a79b451331654
Dima Zhiltsov <zhiltsovdima@gmail.com>
2023-01-13 18:47:25 +0300

    EditPlantCoordinator implementation

---
+++ Secret Garden/Modules/Garden/EditPlant/EditPlantCoordinator.swift
@@ -0,0 +1,40 @@
+//
+//  EditPlantCoordinator.swift
+//  Secret Garden
+//
+//  Created by Dima Zhiltsov on 13.01.2023.
+//
+
+import UIKit.UINavigationController
+
+final class EditPlantCoordinator: Coordinator {
+    
+    private(set) var childCoordinators: [Coordinator] = []
+    
+    private let navigationController: UINavigationController
+    private let garden: Garden
+    private let indexPath: IndexPath
+        
+    init(_ navigationController: UINavigationController, _ garden: Garden,_ indexPath: IndexPath) {
+        self.navigationController = navigationController
+        self.garden = garden
+        self.indexPath = indexPath
+    }
+    
+    func start() {
+        let viewModel = EditPlantViewModel(output: self, garden, indexPath)
+        let editView = EditPlantViewController(viewModel: viewModel)
+        navigationController.dismiss(animated: true)
+        navigationController.present(editView, animated: true)
+    }
+    
+}
+
+extension EditPlantCoordinator: EditPlantOutput {
+    
+    func succesEditing() {
+        navigationController.dismiss(animated: true)
+    }
+    
+
+}
