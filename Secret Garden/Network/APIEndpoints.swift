1b38550e28cf1831be27a1298ac01db71955d88b
Dima Zhiltsov <zhiltsovdima@gmail.com>
2023-05-04 21:40:53 +0300

    Added weather APIEndpoint

--- Secret Garden/Network/APIEndpoints.swift
+++ Secret Garden/Network/APIEndpoints.swift
@@ -8,39 +8,66 @@
 import Foundation
 
 enum APIEndpoints {
-    case common
-    case latin
-    case all
     
-    private var stringURL: String {
-        "https://house-plants.p.rapidapi.com"
+    case plants(PlantsOption)
+    case weather(String)
+    
+    enum PlantsOption {
+        case common(String)
+        case latin(String)
+        case all
     }
-    private var headers: [String: String] {
-        ["X-RapidAPI-Key": PrivateKeys.APIPlantKey,
-         "X-RapidAPI-Host": "house-plants.p.rapidapi.com"
-        ]
+    
+    private var baseURL: String {
+        switch self {
+        case .plants:
+            return "https://house-plants.p.rapidapi.com"
+        case .weather:
+            return "https://api.openweathermap.org/data/2.5/weather"
+        }
     }
     
     private var path: String {
         switch self {
-        case .common: return "/common/"
-        case .latin: return "/latin/"
-        case .all: return "/all"
+        case .plants(let option):
+            switch option {
+            case .common(let plant):
+                let plantName = plant.lowercased().replacingOccurrences(of: " ", with: "")
+                return "/common/\(plantName)"
+            case .latin(let plant):
+                let plantName = plant.lowercased().replacingOccurrences(of: " ", with: "")
+                return "/latin/\(plantName)"
+            case .all: return "/all"
+            }
+        case .weather(let location):
+            return location + "&appid=" + PrivateKeys.APIWeatherKey
+        }
+    }
+    
+    private var headers: [String: String] {
+        switch self {
+        case .plants:
+            return ["X-RapidAPI-Key": PrivateKeys.APIPlantKey,
+                    "X-RapidAPI-Host": "house-plants.p.rapidapi.com"
+            ]
+        case .weather:
+            return ["appid": PrivateKeys.APIWeatherKey]
         }
     }
 }
 
 extension APIEndpoints {
     
-    func makeURLRequest(_ plant: String?) -> URLRequest {
-        var fullPath = path
-        if let plant {
-            fullPath.append(plant.lowercased().replacingOccurrences(of: " ", with: ""))
+    func makeURLRequest() -> URLRequest {
+        var urlString = baseURL
+        urlString.append(path)
+        if let url = URL(string: urlString) {
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            request.allHTTPHeaderFields = headers
+            return request
+        } else {
+            fatalError("Invalid URL")
         }
-        let url = URL(string: fullPath, relativeTo: URL(string: stringURL))
-        var request = URLRequest(url: url!)
-        request.httpMethod = "GET"
-        request.allHTTPHeaderFields = headers
-        return request
     }
 }
